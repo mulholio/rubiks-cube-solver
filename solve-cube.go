c38b059decc352d4815a871e55ac0911b21b3623
James Mulholland <james@jmulholland.com>
2021-07-31 16:01:22 +0100

    define basic cube matrix type

--- solve-cube.go
+++ solve-cube.go
@@ -4,6 +4,40 @@ import (
 	"fmt"
 )
 
+type color int
+
+const (
+	R color = iota
+	G
+	B
+	W
+	Y
+	O
+)
+
+type Cube = [6][3][3]color
+
+var c = Cube{
+	{{R, R, R},
+		{R, R, R},
+		{R, R, R}},
+	{{G, G, G},
+		{G, G, G},
+		{G, G, G}},
+	{{B, B, B},
+		{B, B, B},
+		{B, B, B}},
+	{{W, W, W},
+		{W, W, W},
+		{W, W, W}},
+	{{Y, Y, Y},
+		{Y, Y, Y},
+		{Y, Y, Y}},
+	{{O, O, O},
+		{O, O, O},
+		{O, O, O}},
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println(c)
 }
