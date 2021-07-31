e51471e43efb454195d50d34a500a27398d3c414
James Mulholland <james@jmulholland.com>
2021-07-31 16:49:47 +0100

    visually format a cube into a net

--- solve-cube.go
+++ solve-cube.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 type color int
@@ -16,48 +15,77 @@ const (
 	O
 )
 
+func (c color) String() string {
+	var lookup = [6]string{"R", "G", "B", "W", "Y", "O"}
+	return lookup[c]
+}
+
 /*
 A matrix of faces, representing a cube. Each face is a 3x3 matrix of colour
 values
 
 We number the faces of the cube like so:
 
-    1
-  2 3 4
+    0
+  1 2 3
+    4
     5
-    6
 
 */
 type Cube [6][3][3]color
 
 func (c Cube) String() string {
 	var res string
-	for _, face := range c {
-		for _, row := range face {
-			for _, color := range row {
-				res += strconv.Itoa(int(color))
+
+	res += "\n"
+	formatFaceInCenter(&res, c[0])
+
+	row2 := c[1:4]
+	for i := 0; i < 3; i++ {
+		for _, face := range row2 {
+			for _, color := range face[i] {
+				res += " "
+				res += color.String()
 				res += " "
 			}
-			res += "\n"
+			res += " "
 		}
 		res += "\n"
 	}
+	res += "\n"
+
+	formatFaceInCenter(&res, c[4])
+	formatFaceInCenter(&res, c[5])
+
 	return res
 }
 
+func formatFaceInCenter(res *string, face [3][3]color) {
+	for _, row := range face {
+		*res += "          "
+		for _, color := range row {
+			*res += " "
+			*res += color.String()
+			*res += " "
+		}
+		*res += "\n"
+	}
+	*res += "\n"
+}
+
 var c = Cube{
-	{{R, R, R},
-		{R, R, R},
-		{R, R, R}},
-	{{G, G, G},
-		{G, G, G},
-		{G, G, G}},
-	{{B, B, B},
-		{B, B, B},
-		{B, B, B}},
-	{{W, W, W},
-		{W, W, W},
-		{W, W, W}},
+	{{R, G, B},
+		{R, G, B},
+		{R, G, B}},
+	{{R, G, B},
+		{R, G, B},
+		{R, G, B}},
+	{{R, G, B},
+		{R, G, B},
+		{R, G, B}},
+	{{R, G, B},
+		{R, G, B},
+		{R, G, B}},
 	{{Y, Y, Y},
 		{Y, Y, Y},
 		{Y, Y, Y}},
@@ -65,6 +93,27 @@ var c = Cube{
 		{O, O, O},
 		{O, O, O}},
 }
+
+// var c = Cube{
+// 	{{R, R, R},
+// 		{R, R, R},
+// 		{R, R, R}},
+// 	{{G, G, G},
+// 		{G, G, G},
+// 		{G, G, G}},
+// 	{{B, B, B},
+// 		{B, B, B},
+// 		{B, B, B}},
+// 	{{W, W, W},
+// 		{W, W, W},
+// 		{W, W, W}},
+// 	{{Y, Y, Y},
+// 		{Y, Y, Y},
+// 		{Y, Y, Y}},
+// 	{{O, O, O},
+// 		{O, O, O},
+// 		{O, O, O}},
+// }
 
 func main() {
 	fmt.Println(c)
